f0c51ca4d47234c21fe4193bc8014daa27ff8058
Moria Hamami <moriahamami@gmail.com>
2023-03-28 13:11:22 +0300

    Added initial files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCc3hrCNG86uLY6kxYPhVJg2QNYBr3HT7Q",
+    authDomain: "wazapp-fc40f.firebaseapp.com",
+    projectId: "wazapp-fc40f",
+    storageBucket: "wazapp-fc40f.appspot.com",
+    messagingSenderId: "472922232251",
+    appId: "1:472922232251:web:047c5413282f94e848209e",
+    measurementId: "G-0008QY6TLF"
+}
