257112427d58efd3ac7265bc58711107cb66ab99
Moria Hamami <moriahamami@gmail.com>
2023-05-18 18:12:50 +0300

    add scroll to bottom with every msgs change

--- src/cmps/chat/msg-list.jsx
+++ src/cmps/chat/msg-list.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useRef } from "react";
 import MsgPreview from "./msg-preview"
 
 function MsgList({msgs}) {
+
+    const bottomRef = useRef(null);
+
+    useEffect(() => {
+        // 👇️ scroll to bottom every time messages change
+        bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+      }, [msgs]);
 
     return (
         <article className="msg-list">
             {msgs && msgs.map(msg => (
                 <MsgPreview name={msg.name} msg={msg.msg} timestamp={msg.timestamp} />
             ))}
+            <div ref={bottomRef} />
         </article>
     )
 }
--- src/cmps/chat/msg-preview.jsx
+++ src/cmps/chat/msg-preview.jsx
@@ -4,16 +4,16 @@ function ChatPreview({ timestamp, name, msg }) {
     const loggedInUser = useSelector(storeState => storeState.userModule.user)
 
     return (
-    <>
-        <div className={`rect ${name === loggedInUser.displayName && 'reciever'}`}></div>
-        <div className={`chat-msg ${name === loggedInUser.displayName && 'reciever'}`}>
-            <p className="username">{name}</p>
-            <p className="content">{msg}</p>
-            <p className="timestamp">{new Date(timestamp?.toDate()).toUTCString()}</p>
+        <div className="msg-preview">
+            <div className={`rect ${name === loggedInUser.displayName && 'reciever'}`}></div>
+            <div className={`chat-msg ${name === loggedInUser.displayName && 'reciever'}`}>
+                <p className="username">{name}</p>
+                <p className="content">{msg}</p>
+                <p className="timestamp">{new Date(timestamp?.toDate()).toUTCString()}</p>
+            </div>
         </div>
-        {/* <div className={`circle ${name === loggedInUser.displayName && 'reciever'}`}></div> */}
-    </>
     )
+    {/* <div className={`circle ${name === loggedInUser.displayName && 'reciever'}`}></div> */ }
 }
 
 export default ChatPreview
