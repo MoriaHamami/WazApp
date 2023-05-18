b5a961b5efa84baf7d292145cafaa8cc42569234
Moria Hamami <moriahamami@gmail.com>
2023-05-18 17:47:59 +0300

    change msg appearance

--- src/cmps/chat/msg-preview.jsx
+++ src/cmps/chat/msg-preview.jsx
@@ -5,13 +5,13 @@ function ChatPreview({ timestamp, name, msg }) {
 
     return (
     <>
+        <div className={`rect ${name === loggedInUser.displayName && 'reciever'}`}></div>
         <div className={`chat-msg ${name === loggedInUser.displayName && 'reciever'}`}>
             <p className="username">{name}</p>
             <p className="content">{msg}</p>
             <p className="timestamp">{new Date(timestamp?.toDate()).toUTCString()}</p>
         </div>
-        <div className={`circle ${name === loggedInUser.displayName && 'reciever'}`}></div>
-        <div className={`rect ${name === loggedInUser.displayName && 'reciever'}`}></div>
+        {/* <div className={`circle ${name === loggedInUser.displayName && 'reciever'}`}></div> */}
     </>
     )
 }
